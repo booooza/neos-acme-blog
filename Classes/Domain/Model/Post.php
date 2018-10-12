e3ef77c0aad30727056315f5ff6c6e2336137def
Marc Bischof <marc.bischof@swisscom.com>
2018-10-12 14:13:57 +0200

    Add post model

--- Classes/Domain/Model/Post.php
+++ Classes/Domain/Model/Post.php
@@ -1,9 +1,10 @@
 <?php
 namespace Acme\Blog\Domain\Model;
 
-/*
- * This file is part of the Acme.Blog package.
- */
+/*                                                                        *
+ * This script belongs to the Flow package "Acme.Blog".                   *
+ *                                                                        *
+ *                                                                        */
 
 use Neos\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
@@ -11,29 +12,122 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @Flow\Entity
  */
-class Post
-{
+class Post {
 
-    /**
-     * @var string
-     */
-    protected $name;
+        /**
+         * @Flow\Validate(type="NotEmpty")
+         * @ORM\ManyToOne(inversedBy="posts")
+         * @var Blog
+         */
+        protected $blog;
 
+        /**
+         * @Flow\Validate(type="NotEmpty")
+         * @var string
+         */
+        protected $subject;
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+        /**
+         * The creation date of this post (set in the constructor)
+         *
+         * @var \DateTime
+         */
+        protected $date;
 
-    /**
-     * @param string $name
-     * @return void
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
+        /**
+         * @Flow\Validate(type="NotEmpty")
+         * @var string
+         */
+        protected $author;
+
+        /**
+         * @Flow\Validate(type="NotEmpty")
+         * @ORM\Column(type="text")
+         * @var string
+         */
+        protected $content;
+
+        /**
+         * Constructs this post
+         */
+        public function __construct() {
+                $this->date = new \DateTime();
+        }
+
+        /**
+         * @return Blog
+         */
+        public function getBlog() {
+                return $this->blog;
+        }
+
+        /**
+         * @param Blog $blog
+         * @return void
+         */
+        public function setBlog(Blog $blog) {
+                $this->blog = $blog;
+                $this->blog->addPost($this);
+        }
+
+        /**
+         * @return string
+         */
+        public function getSubject() {
+                return $this->subject;
+        }
+
+        /**
+         * @param string $subject
+         * @return void
+         */
+        public function setSubject($subject) {
+                $this->subject = $subject;
+        }
+
+        /**
+         * @return \DateTime
+         */
+        public function getDate() {
+                return $this->date;
+        }
+
+        /**
+         * @param \DateTime $date
+         * @return void
+         */
+        public function setDate(\DateTime $date) {
+                $this->date = $date;
+        }
+
+        /**
+         * @return string
+         */
+        public function getAuthor() {
+                return $this->author;
+        }
+
+        /**
+         * @param string $author
+         * @return void
+         */
+        public function setAuthor($author) {
+                $this->author = $author;
+        }
+
+        /**
+         * @return string
+         */
+        public function getContent() {
+                return $this->content;
+        }
+
+        /**
+         * @param string $content
+         * @return void
+         */
+        public function setContent($content) {
+                $this->content = $content;
+        }
+
 }
