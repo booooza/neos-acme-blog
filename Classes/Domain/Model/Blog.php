b6bce4b67bece9e2a966a5a864c0967526ec546b
Marc Bischof <marc.bischof@swisscom.com>
2018-10-12 14:13:41 +0200

    Add blog model

--- Classes/Domain/Model/Blog.php
+++ Classes/Domain/Model/Blog.php
@@ -1,81 +1,110 @@
 <?php
 namespace Acme\Blog\Domain\Model;
 
-/*
- * This file is part of the Acme.Blog package.
- */
+/*                                                                        *
+ * This script belongs to the Flow package "Acme.Blog".                   *
+ *                                                                        *
+ *                                                                        */
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Neos\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * A blog that contains a list of posts
+ *
  * @Flow\Entity
  */
-class Blog
-{
+class Blog {
 
-    /**
-     * @var string
-     */
-    protected $title;
+        /**
+         * @Flow\Validate(type="NotEmpty")
+         * @Flow\Validate(type="StringLength", options={ "minimum"=3, "maximum"=80 })
+         * @ORM\Column(length=80)
+         * @var string
+         */
+        protected $title;
 
-    /**
-     * @var string
-     */
-    protected $description;
+        /**
+         * @Flow\Validate(type="StringLength", options={ "maximum"=150 })
+         * @ORM\Column(length=150)
+         * @var string
+         */
+        protected $description = '';
 
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    protected $posts;
+        /**
+         * The posts contained in this blog
+         *
+         * @ORM\OneToMany(mappedBy="blog")
+         * @ORM\OrderBy({"date" = "DESC"})
+         * @var Collection<Post>
+         */
+        protected $posts;
 
+        /**
+         * @param string $title
+         */
+        public function __construct($title) {
+                $this->posts = new ArrayCollection();
+                $this->title = $title;
+        }
 
-    /**
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
+        /**
+         * @return string
+         */
+        public function getTitle() {
+                return $this->title;
+        }
 
-    /**
-     * @param string $title
-     * @return void
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
-    /**
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
+        /**
+         * @param string $title
+         * @return void
+         */
+        public function setTitle($title) {
+                $this->title = $title;
+        }
 
-    /**
-     * @param string $description
-     * @return void
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-    }
-    /**
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getPosts()
-    {
-        return $this->posts;
-    }
+        /**
+         * @return string
+         */
+        public function getDescription() {
+                return $this->description;
+        }
 
-    /**
-     * @param \Doctrine\Common\Collections\Collection $posts
-     * @return void
-     */
-    public function setPosts(\Doctrine\Common\Collections\Collection $posts)
-    {
-        $this->posts = $posts;
-    }
+        /**
+         * @param string $description
+         * @return void
+         */
+        public function setDescription($description) {
+                $this->description = $description;
+        }
+
+        /**
+         * @return Collection
+         */
+        public function getPosts() {
+                return $this->posts;
+        }
+
+        /**
+         * Adds a post to this blog
+         *
+         * @param Post $post
+         * @return void
+         */
+        public function addPost(Post $post) {
+                $this->posts->add($post);
+        }
+
+        /**
+         * Removes a post from this blog
+         *
+         * @param Post $post
+         * @return void
+         */
+        public function removePost(Post $post) {
+                $this->posts->removeElement($post);
+        }
+
 }
